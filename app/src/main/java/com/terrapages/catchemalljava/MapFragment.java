1e2025500f1d6d2ec47501258d39062d56900b25
iquesp <dennis.yuan@terrapages.com>
2018-01-30 17:45:11 +1100

    2. Overlaying Views

--- app/src/main/java/com/terrapages/catchemalljava/MapFragment.java
+++ app/src/main/java/com/terrapages/catchemalljava/MapFragment.java
@@ -1,150 +1,240 @@
 package com.terrapages.catchemalljava;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.util.Log;
-import android.view.LayoutInflater;
+import android.support.annotation.Nullable;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.SeekBar;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.places.GeoDataClient;
-import com.google.android.gms.location.places.PlaceDetectionClient;
-import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
+import com.google.android.gms.maps.model.IndoorBuilding;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback {
+import java.io.IOException;
 
-    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
-    private static final int DEFAULT_ZOOM = 15;
-    private static final String TAG = MapFragment.class.getSimpleName();
-    private final LatLng mDefaultLocation = new LatLng(-33.8523341, 151.2106085);
+public class MapFragment extends SupportMapFragment implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        GoogleMap.OnInfoWindowClickListener,
+        GoogleMap.OnMapLongClickListener,
+        GoogleMap.OnMapClickListener,
+        GoogleMap.OnMarkerClickListener,
+        GoogleMap.OnIndoorStateChangeListener,
+        SeekBar.OnSeekBarChangeListener {
 
-    private GoogleMap mMap;
-    private GeoDataClient mGeoDataClient;
-    private PlaceDetectionClient mPlaceDetectionClient;
-    private FusedLocationProviderClient mFusedLocationProviderClient;
-    private boolean mLocationPermissionGranted;
-    private Location mLastKnownLocation;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mCurrentLocation;
+    private GoogleMap mGoogleMap;
 
-    public MapFragment() {
-        // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        mGeoDataClient = Places.getGeoDataClient(this.getContext(), null);
-        mPlaceDetectionClient = Places.getPlaceDetectionClient(this.getContext(), null);
-        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this.getContext());
-    }
+    private final int[] MAP_TYPES = {
+            GoogleMap.MAP_TYPE_SATELLITE,
+            GoogleMap.MAP_TYPE_NORMAL,
+            GoogleMap.MAP_TYPE_HYBRID,
+            GoogleMap.MAP_TYPE_TERRAIN,
+            GoogleMap.MAP_TYPE_NONE };
+    private int curMapTypeIndex = 2;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_map, container, false);
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-        return view;
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        setHasOptionsMenu(true);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        getLocationPermission();
-        updateLocationUI();
-        getDeviceLocation();
+        mGoogleMap = googleMap;
+        // initListener
+        mGoogleMap.setOnMarkerClickListener(this);
+        mGoogleMap.setOnMapLongClickListener(this);
+        mGoogleMap.setOnInfoWindowClickListener(this);
+        mGoogleMap.setOnMapClickListener(this);
     }
 
-    private void getLocationPermission() {
-        if (ContextCompat.checkSelfPermission(this.getContext().getApplicationContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            mLocationPermissionGranted = true;
-        } else {
-            ActivityCompat.requestPermissions(this.getActivity(),
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+    private void initCamera(Location location) {
+        CameraPosition position = CameraPosition.builder()
+                .target(new LatLng(location.getLatitude(), location.getLongitude()))
+                .zoom(18f)
+                .bearing(0.0f)
+                .tilt(0.0f)
+                .build();
+
+        mGoogleMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(position), null);
+        mGoogleMap.setMapType(MAP_TYPES[curMapTypeIndex]);
+        mGoogleMap.setTrafficEnabled(true);
+        mGoogleMap.setMyLocationEnabled(true);
+        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        mLocationPermissionGranted = false;
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    mLocationPermissionGranted = true;
-                } else {
-                    //
-                }
-            }
-        }
-        updateLocationUI();
+    public void onConnected(@Nullable Bundle bundle) {
+        mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+//        initCamera(mCurrentLocation);
+        Location location = new Location("Madison Square Garden");
+        location.setLatitude(40.7506);
+        location.setLongitude(-73.9936);
+        initCamera(location);
     }
 
-    private void updateLocationUI() {
-        if (mMap == null) {
-            return;
-        }
-        try {
-            if (mLocationPermissionGranted) {
-                mMap.setMyLocationEnabled(true);
-                mMap.getUiSettings().setMyLocationButtonEnabled(true);
-                mMap.getUiSettings().setZoomControlsEnabled(true);
-            } else {
-                mMap.setMyLocationEnabled(false);
-                mMap.getUiSettings().setMyLocationButtonEnabled(false);
-                mMap.getUiSettings().setZoomControlsEnabled(false);
-                mLastKnownLocation = null;
-                getLocationPermission();
-            }
-        } catch (SecurityException e) {
-            Log.e("Exception: %s", e.getMessage());
-        }
+    @Override
+    public void onConnectionSuspended(int i) {
+
     }
 
-    private void getDeviceLocation() {
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+
+    }
+
+    private String getAddressFromLatLng(LatLng latLng) {
+        Geocoder geocoder = new Geocoder(getActivity());
+
+        String address = "";
         try {
-            if (mLocationPermissionGranted) {
-                Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
-                locationResult.addOnCompleteListener(this.getActivity(), new OnCompleteListener<Location>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Location> task) {
-                        if (task.isSuccessful()) {
-                            mLastKnownLocation = task.getResult();
-                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                                    new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()),
-                                    DEFAULT_ZOOM
-                            ));
-                        } else {
-                            Log.d(TAG, "Current location is null. Using defaults.");
-                            Log.e(TAG, "Exception: %s", task.getException());
-                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
-                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
-                        }
-                    }
-                });
-            }
-        } catch (SecurityException e) {
-            Log.e("Exception: %s", e.getMessage());
+            address = geocoder
+                    .getFromLocation(latLng.latitude, latLng.longitude, 1)
+                    .get(0).getAddressLine(0);
+        } catch (IOException e) {
+            //
         }
+
+        return address;
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        MarkerOptions options = new MarkerOptions().position(latLng);
+        options.title(getAddressFromLatLng(latLng));
+
+        options.icon(BitmapDescriptorFactory.defaultMarker());
+        mGoogleMap.addMarker(options);
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        MarkerOptions options = new MarkerOptions().position(latLng);
+        options.title(getAddressFromLatLng(latLng));
+
+        options.icon(BitmapDescriptorFactory.fromBitmap(
+                BitmapFactory.decodeResource(getResources(),
+                        R.mipmap.ic_launcher_round)));
+        mGoogleMap.addMarker(options);
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        marker.showInfoWindow();
+        return true;
+    }
+
+    /** helper methods **/
+    private void drawCircle(LatLng location) {
+        CircleOptions options = new CircleOptions();
+        options.center(location);
+
+        options.radius(10);
+        options.fillColor(getResources()
+                .getColor(R.color.fill_color));
+        options.strokeColor(getResources()
+                .getColor(R.color.stroke_color));
+        options.strokeWidth(10);
+        mGoogleMap.addCircle(options);
+    }
+
+    private void drawPolygon(LatLng startingLocation) {
+        LatLng point2 = new LatLng(startingLocation.latitude + .001,
+                startingLocation.longitude);
+        LatLng point3 = new LatLng(startingLocation.latitude,
+                startingLocation.longitude + .001);
+
+        PolygonOptions options = new PolygonOptions();
+        options.add(startingLocation, point2, point3);
+
+        options.fillColor(getResources()
+                .getColor(R.color.fill_color));
+        options.strokeColor(getResources()
+                .getColor(R.color.stroke_color));
+        options.strokeWidth(10);
+
+        mGoogleMap.addPolygon(options);
+    }
+
+    private void drawOverlay(LatLng location, int width, int height) {
+        GroundOverlayOptions options = new GroundOverlayOptions();
+        options.position(location, width, height);
+
+        options.image(BitmapDescriptorFactory
+                .fromBitmap(BitmapFactory
+                        .decodeResource(getResources(),
+                                R.mipmap.ic_launcher)));
+        mGoogleMap.addGroundOverlay(options);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onIndoorBuildingFocused() {
+
+    }
+
+    @Override
+    public void onIndoorLevelActivated(IndoorBuilding indoorBuilding) {
+
     }
 }
