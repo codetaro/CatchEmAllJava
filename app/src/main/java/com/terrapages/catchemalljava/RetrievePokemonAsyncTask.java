dc1692615c19aa6013dd2c0c873e4fb56769072c
iquesp <dennis.yuan@terrapages.com>
2018-02-06 18:44:08 +1100

    Style marker with picture of pokemon

--- app/src/main/java/com/terrapages/catchemalljava/RetrievePokemonAsyncTask.java
+++ app/src/main/java/com/terrapages/catchemalljava/RetrievePokemonAsyncTask.java
@@ -7,6 +7,9 @@ import android.location.Location;
 import android.os.AsyncTask;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -15,7 +18,7 @@ import me.sargunvohra.lib.pokekotlin.client.PokeApi;
 import me.sargunvohra.lib.pokekotlin.client.PokeApiClient;
 import me.sargunvohra.lib.pokekotlin.model.Pokemon;
 
-public class RetrievePokemonAsyncTask extends AsyncTask<Void, Void, ArrayList> {
+public class RetrievePokemonAsyncTask extends AsyncTask<Void, Void, ArrayList<Pokemon>> {
 
     private Context mContext;
 
@@ -41,7 +44,7 @@ public class RetrievePokemonAsyncTask extends AsyncTask<Void, Void, ArrayList> {
     }
 
     @Override
-    protected ArrayList doInBackground(Void... voids) {
+    protected ArrayList<Pokemon> doInBackground(Void... voids) {
         PokeApi pokeApi = new PokeApiClient();
         Pokemon pokemon;
 
@@ -59,9 +62,27 @@ public class RetrievePokemonAsyncTask extends AsyncTask<Void, Void, ArrayList> {
     }
 
     @Override
-    protected void onPostExecute(ArrayList arrayList) {
+    protected void onPostExecute(ArrayList<Pokemon> pokemons) {
         progressDialog.dismiss();
 
         // Populate the map with pokemon
+        double lat;
+        double lng;
+        Marker pMarker;
+        Random generator = new Random();
+        for (Pokemon p : pokemons) {
+            lat = generator.nextDouble() / 300;
+            lng = generator.nextDouble() / 300;
+            if (generator.nextBoolean()) {
+                lat = -lat;
+            }
+            if (generator.nextBoolean()) {
+                lng = -lng;
+            }
+            pMarker = mGoogleMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(mCurrentLocation.getLatitude() + lat, mCurrentLocation.getLongitude() + lng))
+                    .title(p.getName()));
+            pMarker.setTag(p);
+        }
     }
 }
